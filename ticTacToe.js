31813c632c9283c33a411f36ae18454aa43dec43
msilis <mikssilis@gmail.com>
2022-12-20 10:22:26 +0000

    updated logic

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,6 +1,6 @@
 // Define arrays
 const rowWin = [
-    ["O", "0", "O"],
+    ["O", "O", "O"],
     ["", "", ""],
     ["", "", ""]
 ]
@@ -28,32 +28,43 @@ const diagonalWinInverse = [
 // Your program must be able to evaluate all the grids defined above.
 // The function should inform the user of which sign has  won and which sign has lost
 // You may add additional parameters to assist you
+
 function evaluatePlay(grid){
 
 
-     for (let i = 0; i<grid.length; i++){
-        for (let j = 0; j<grid[i].length; j++){
-            if (grid[i][0] && grid[i][1] && grid[i][2] == "O"){
-            console.log('O wins\nX loses')
-        }else if ( grid[0][1] && grid[1][1] && grid[2][1] == "X")
-        {console.log('X wins\nO loses')
-            
-        }else if(grid[0][2] && grid[1][1] && grid[2][0] == "O"){
-            console.log('0 wins\nX loses')
-        }else if(grid[0][0] && grid[1][1] && grid[2][2] == "X"){
-            console.log('X wins\nO loses')
-        }
-    }
-    } 
+    for (let i = 0; i<grid.length; i++){
+       for (let j = 0; j<grid[i].length; j++){
+    //Horizontal win
+           if (grid[i][0] == grid[i][1] && grid [i][0] == grid[i][2]){
+            if(grid[i][0] == 'O'){
+                console.log('O wins\nX loses')
+                break
+            }else console.log('X wins\nO loses')
+    //Vertical win
+       }else if ( grid[0][1] == grid[1][1] && grid[0][1] == grid[2][1])
+        { if(grid[0][1] == 'X') console.log('X wins\nO loses')
+            else console.log('O wins\nX loses')
+    //Diagonal win
+       }else if(grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0]){
+            if(grid[1][1] == 'O') console.log('0 wins\nX loses')
+                else console.log('X wins\nO loses')
+    //Inverse Diagonal win
+       }else if(grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]){
+            if (grid[1][1] == 'X') console.log('X wins\nO loses')
+                else onsole.log('O wins\nX loses')
+       }
+   }
+   } 
 
-   
+  
 
 
- 
-    // ------------------------------- Have -------------------------------------------
 
-    // ------------------------------- Fun  -------------------------------------------
+   // ------------------------------- Have -------------------------------------------
+
+   // ------------------------------- Fun  -------------------------------------------
 }
+
 
 console.log(evaluatePlay(rowWin))
 console.log(evaluatePlay(colWin))
